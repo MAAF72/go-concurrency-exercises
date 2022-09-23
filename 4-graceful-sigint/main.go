68d3a9fb4813c61e2154e9210b35d8f292996c71
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-09-23 19:15:06 +0700

    ans(05): complete the exercise

--- 4-graceful-sigint/main.go
+++ 4-graceful-sigint/main.go
@@ -13,9 +13,49 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT)
+
+	done := make(chan bool, 1)
+
 	// Create a process
 	proc := MockProcess{}
+
+	go func() {
+		sig := <-sigs
+
+		if sig == syscall.SIGINT {
+			fmt.Println("try to gracefully stop")
+			go func() {
+				proc.Stop()
+				done <- true
+			}()
+
+			go func() {
+				sig := <-sigs
+
+				if sig == syscall.SIGINT {
+					done <- false
+				}
+			}()
+		}
+
+		if status := <-done; status {
+			fmt.Println("gracefully stopped")
+			os.Exit(0)
+		} else {
+			fmt.Println("process killed")
+			os.Exit(1)
+		}
+	}()
 
 	// Run the process (blocking)
 	proc.Run()
